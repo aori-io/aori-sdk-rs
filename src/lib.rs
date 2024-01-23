4712185a7a71d2f1696f5af6e87029f47e6b9f34
anticode <146665183+anticode-dev@users.noreply.github.com>
2024-01-23 12:45:45 +0100

    feat: aoriOrder with a custom de/serializer

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,12 @@ pub mod client;
 pub mod error;
 pub mod request;
 pub mod response;
+pub mod serialize;
 pub mod shared_types;
 
 pub use client::AoriBackendRpcClient;
 pub use error::AoriBackendErrors;
 pub use request::*;
 pub use response::*;
+pub use serialize::*;
 pub use shared_types::*;
--- src/shared_types.rs
+++ src/shared_types.rs
@@ -1,23 +1,36 @@
-// here goes aori order and others
+use crate::*; 
+use alloy_sol_types::sol;
 use serde::{Deserialize, Serialize};
 
-#[derive(Default, Serialize, Deserialize, Debug)]
-pub struct AoriOrder {
-    pub offerer: String,
-    pub input_token: String,
-    pub input_amount: String,
-    pub input_chain_id: i64,
-    pub input_zone: String,
-    pub output_token: String,
-    pub output_amount: String,
-    pub output_chain_id: i64,
-    pub output_zone: String,
-    pub start_time: String,
-    pub end_time: String,
-    pub salt: String,
-    pub counter: i64,
-    pub to_withdraw: bool,
-}
+sol!(
+    #[derive(Default, Debug, Deserialize, Serialize)]
+    struct AoriOrder {
+        address offerer;
+        // input
+        address inputToken;
+        #[serde(serialize_with = "serialize_u256_as_string", deserialize_with = "deserialize_u256_from_string")]
+        uint256 inputAmount;
+        #[serde(serialize_with = "serialize_u256_as_u32", deserialize_with = "deserialize_u256_from_u32")]
+        uint256 inputChainId;
+        address inputZone;
+        // output
+        address outputToken;
+        #[serde(serialize_with = "serialize_u256_as_string", deserialize_with = "deserialize_u256_from_string")]
+        uint256 outputAmount;
+        #[serde(serialize_with = "serialize_u256_as_u32", deserialize_with = "deserialize_u256_from_u32")]
+        uint256 outputChainId;
+        address outputZone;
+        // other
+        #[serde(serialize_with = "serialize_u256_as_string", deserialize_with = "deserialize_u256_from_string")]
+        uint256 startTime;
+        #[serde(serialize_with = "serialize_u256_as_string", deserialize_with = "deserialize_u256_from_string")]
+        uint256 endTime;
+        uint256 salt;
+        #[serde(serialize_with = "serialize_u256_as_u32", deserialize_with = "deserialize_u256_from_u32")]
+        uint256 counter;
+        bool toWithdraw;
+    }
+);
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AoriMatchingDetails {
@@ -105,4 +118,66 @@ pub struct DetailsToExecute {
     pub input_amount: String,
     pub output_token: String,
     pub output_amount: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy_primitives::{keccak256, Address, U256};
+    use alloy_sol_types::SolValue;
+    use serde_json;
+    #[test]
+    fn sign_order() {
+        let order = AoriOrder {
+            offerer: "0x0000000000000000000000000000000000000001".parse::<Address>().unwrap(),
+            inputToken: "0x0000000000000000000000000000000000000002".parse::<Address>().unwrap(),
+            inputAmount: U256::from(1000000000000000000_u64),
+            inputChainId: U256::from(1),
+            inputZone: "0x0000000000000000000000000000000000000003".parse::<Address>().unwrap(),
+            outputToken: "0x0000000000000000000000000000000000000004".parse::<Address>().unwrap(),
+            outputAmount: U256::from(2000000000000000000_u64),
+            outputChainId: U256::from(1),
+            outputZone: "0x0000000000000000000000000000000000000000".parse::<Address>().unwrap(),
+            startTime: U256::from(1619827200),
+            endTime: U256::from(1622428800),
+            salt: U256::from(1),
+            counter: U256::from(1),
+            toWithdraw: false,
+        };
+        let packed = order.abi_encode_packed();
+        let hash = keccak256(packed);
+        println!("rust order hash {}", hash);
+        assert_eq!(
+            hash.to_string(),
+            "0x214356d7e7b271d965916a29d61e111e8106f54a2b805a742c18bf93f9f2372e",
+            "Order hash does not match expected value"
+        );
+    }
+
+    #[test]
+    fn serialize_aori_order() {
+        let order = AoriOrder {
+            offerer: "0x0000000000000000000000000000000000000001".parse::<Address>().unwrap(),
+            inputToken: "0x0000000000000000000000000000000000000002".parse::<Address>().unwrap(),
+            inputAmount: U256::from(1000000000000000000_u64),
+            inputChainId: U256::from(1),
+            inputZone: "0x0000000000000000000000000000000000000003".parse::<Address>().unwrap(),
+            outputToken: "0x0000000000000000000000000000000000000004".parse::<Address>().unwrap(),
+            outputAmount: U256::from(2000000000000000000_u64),
+            outputChainId: U256::from(1),
+            outputZone: "0x0000000000000000000000000000000000000000".parse::<Address>().unwrap(),
+            startTime: U256::from(1619827200),
+            endTime: U256::from(1622428800),
+            salt: U256::from(1),
+            counter: U256::from(1),
+            toWithdraw: false,
+        };
+
+        let serialized = serde_json::to_string(&order).unwrap();
+        println!("Serialized AoriOrder: {}", serialized);
+
+        // Deserialize the order
+        let deserialized: AoriOrder = serde_json::from_str(&serialized).unwrap();
+        println!("Deserialized AoriOrder: {:?}", deserialized);
+    }
 }
